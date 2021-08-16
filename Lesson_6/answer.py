93ef3ac273f319f06066cba8365f195799eaaa5e
YOSEF GUEVARA <yosef.guevara012@hotmail.com>
2021-08-16 00:00:35 -0500

    OOP syntax practice, part 2

--- Lesson_6/answer.py
+++ Lesson_6/answer.py
@@ -1,57 +1,133 @@
-### TODO:
-#    - import the Shirt class from the shirt.py file
-###
+class Pants:
+    """The Pants class represents an article of clothing sold in a store
+    """
+    
+    def __init__(self, color, waist_size, length, price):
+        """Method for initializing a Pants object
+    
+        Args: 
+            color (str)
+            waist_size (int)
+            length (int)
+            price (float)
+            
+        Attributes:
+            color (str): color of a pants object
+            waist_size (str): waist size of a pants object
+            length (str): length of a pants object
+            price (float): price of a pants object
+        """
+            
+        self.color = color
+        self.waist_size = waist_size
+        self.length = length
+        self.price = price
+    
+    def change_price(self, new_price):
+        """The change_price method changes the price attribute of a pants object
+    
+        Args: 
+            new_price (float): the new price of the pants object
+            
+        Returns: None
+        
+        """
+        self.price = new_price
+    
+    def discount(self, percentage):
+        """The discount method outputs a discounted price of a pants object
 
-from shirt import Shirt
+        Args:
+            percentage (float): a decimal representing the amount to discount
+
+        Returns:
+            float: the discounted price
+        """
+        return self.price * (1 - percentage)
 
 
-### TODO:
-#    - insantiate a shirt object with the following characteristics:
-#        - color red, size S, style long-sleeve, and price 25
-#    - store the object in a variable called shirt_one
-#
-#
-###
-shirt_one = Shirt('red', 'S', 'long-sleeve', 25)
+class SalesPerson:
+    """The SalesPerson class represents an employee in the store
 
+    """
 
-### TODO:
-#     - print the price of the shirt using the price attribute
-#     - change the price of the shirt to be 10
-#     - print the price of the shirt using the price attribute
-#     - print the price of the shirt with a 12% discount
-#
-###
-print(shirt_one.price)
-shirt_one.change_price(10)
-print(shirt_one.price)
-print(shirt_one.discount(.12))
+    def __init__(self, first_name, last_name, employee_id, salary):
+        """Method for initializing a SalesPerson object
+        
+        Args: 
+            first_name (str)
+            last_name (str)
+            employee_id (int)
+            salary (float)
 
+        Attributes:
+            first_name (str): first name of the employee
+            last_name (str): last name of the employee
+            employee_id (int): identification number of the employee
+            salary (float): yearly salary of the employee
+            pants_sold (list): a list of pants objects sold by the employee
+            total_sales (float): sum of all sales made by the employee
 
-### TODO:
-#
-#    - instantiate another object with the following characteristics:
-# .       - color orange, size large, style short sleeve, and price 10
-#    - store the object in a variable called shirt_two
-#
-###
-shirt_two = Shirt('orange', 'L', 'short-sleeve', 10)
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.employee_id = employee_id
+        self.salary = salary
+        self.pants_sold = []
+        self.total_sales = 0
 
+    def sell_pants(self, pants_object):
+        """The sell_pants method appends a pants object to the pants_sold attribute
 
-### TODO:
-#
-#    - calculate the total cost of shirt_one and shirt_two
-#    - store the results in a variable called total
-#    
-###
-total = shirt_one.price + shirt_two.price
+        Args: 
+            pants_object (obj): a pants object that was sold
 
+        Returns: None
 
-### TODO:
-#
-#    - use the shirt discount method to calculate the total cost if
-#       shirt_one has a discount of 14% and shirt_two has a discount
-#       of 6%
-#    - store the results in a variable called total_discount
-###
-total_discount =  shirt_one.discount(.14) + shirt_two.discount(.06) 
+        """
+
+        self.pants_sold.append(pants_object)
+
+    def display_sales(self):
+        """The display_sales method prints out all pants that have been sold
+
+        Args: None
+
+        Returns: None
+
+        """
+
+        for pants in self.pants_sold:
+            print('color: {}, waist_size: {}, length: {}, price: {}'\
+                  .format(pants.color, pants.waist_size, pants.length, pants.price))
+    
+    def calculate_sales(self):
+        """The calculate_sales method sums the total price of all pants sold
+
+        Args: None
+
+        Returns:
+            float: sum of the price for all pants sold
+        
+        """
+
+        total = 0
+        for pants in self.pants_sold:
+            total += pants.price
+            
+        self.total_sales = total
+        
+        return total
+    
+    def calculate_commission(self, percentage):
+        """The calculate_commission method outputs the commission based on sales
+
+        Args:
+            percentage (float): the commission percentage as a decimal
+
+        Returns:
+            float: the commission due
+        """
+
+        sales_total = self.calculate_sales()
+        return sales_total * percentage 
